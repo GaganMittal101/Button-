2fac9d2d6a43e8cc2c13e10b6766afc7d272506c
gaganmittal101 <gaganmittal925@gmail.com>
2024-07-18 14:34:25 +0530

    react app

---
+++ src/component/Togglefun.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Funcompone from './Funcompone'
+import Funcomtwo from './Funcomtwo'
+import '../App.css'
+
+const Togglefun = () => {
+    const [funcone , setfuncone] = useState(false);
+    const [functwo , setfunctwo] = useState(false);
+
+
+
+
+
+  return (
+    <div>
+        <button onClick={()=>setfuncone(!funcone)}>First Functional Component</button>
+        <button onClick={()=>setfunctwo(!functwo)}>Second Functional Component</button>
+
+
+    <center><div className="container">
+        {funcone?<Funcompone/>:null}
+        {functwo?<Funcomtwo/>:null}
+    </div></center>
+    
+    </div>
+  )
+}
+
+export default Togglefun
